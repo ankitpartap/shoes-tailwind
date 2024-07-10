17a864c9650d3b07ac2c0cf772a6fbfa47848510
Ankit Partap <84763601+ankitpartap@users.noreply.github.com>
2024-07-10 11:40:41 +0530

    Saving changes in the sections folder

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -4,6 +4,11 @@ import { services } from "../constants";
 const Services = () => {
   return (
     <section>
+      <div className="mb-10">
+        <h2 className="text-4xl font-palanquin font-bold">
+          Our <span className="text-coral-red">Services</span>
+        </h2>
+      </div>
       <div className="max-container flex justify-center flex-wrap gap-9">
         {services.map((service) => (
           <ServiceCard key={service.label} {...service} />
